16cf5fc10fb3659dbe584efecd0684b140d6f2f9
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-03-01 02:03:14 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
+import Header from "./components/Header";
+import Main from "./components/Main";
+import AddCardModal from "./components/AddCardModal";
+
+import './styles/scss/main.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+    const [modalVisibility, setModalVisibility] = useState(false)
+    const [cards, setCards] = useState([])
+
+    const showHideModalClasses = modalVisibility ? 'active' : null
+
+    return (
+        <div className="wrap">
+              <Header/>
+              <Main
+                  setModalVisibility = { setModalVisibility }
+                  cards = { cards }
+              />
+              <AddCardModal
+                  modalVisibility = { modalVisibility }
+                  setModalVisibility = { setModalVisibility }
+                  modalClasses = { showHideModalClasses }
+                  cards = { cards }
+                  setCards = { setCards }
+              />
+        </div>
+    );
+    }
 
 export default App;
