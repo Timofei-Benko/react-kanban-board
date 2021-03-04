973df62352dd74155e22151c1e8268b3c3d6a460
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-03-04 14:43:41 +0300

    refactor: removed anonymous export default

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,13 @@
-export default () => (
+const Footer = () => (
     <footer className="footer">
         <p className="footer__copyright">Made by Timofei Benko</p>
         <p className="footer__github">
-            <a href="https://github.com/Timofei-Benko/" className="footer__github-link">github</a>
+            <a href="https://github.com/Timofei-Benko/"
+               className="footer__github-link"
+            >github
+            </a>
         </p>
     </footer>
 )
+
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
-export default () => (
+const Header = () => (
         <header className="header">
             <h1 className="header__title">Kanban Board</h1>
             <p className="header__subtitle">A board to keep track of your tasks</p>
         </header>
 )
+
+export default Header;
